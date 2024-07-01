ce8ecc9e5c323f156c05e88c830a8c0a6905625c
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2024-07-01 13:06:51 +0200

    docs: adjusting echo's during bootstrapping

--- docs/getting-started/start.sh
+++ docs/getting-started/start.sh
@@ -21,22 +21,18 @@ if [ $(kind get clusters | grep -c openmfp) -gt 0 ]; then
 else
   echo -e "$COL Creating kind cluster $COL_RES"
   kind create cluster --config kind-config.yaml --name openmfp
-    echo -e "$COL Creating kind cluster. Complete $COL_RES"
 fi
 
 # Install flux
 echo "$COL Installing flux $COL_RES"
 flux install --components source-controller,helm-controller
-echo "$COL Installing flux. Complete $COL_RES"
 
 # Prepare installation namespace
 echo "$COL Creating openmfp-system namespace $COL_RES"
 kubectl apply -k ./infrastructure/namespace
 
-echo "$COL creating secret for ghcr.io $COL_RES"
+echo "$COL Creating necessary secrets $COL_RES"
 flux create secret oci ghcr-credentials -n openmfp-system --url ghcr.io --username $(gh api user | jq -r '.login') --password $GH_TOKEN
-
-echo "$COL creating secret for keycloak $COL_RES"
 kubectl create secret generic keycloak-admin -n openmfp-system --from-literal=secret=admin --dry-run=client -o yaml | kubectl apply -f -
 
 
