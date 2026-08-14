1848f01c7a6879ae6a3913cb2c5bf789d2abe279
gkrajniak <gkrajniak@gmail.com>
2026-08-14 12:40:28 +0000

    Deploying to gh-pages from @ openmfp/openmfp.org@05e9df4151ca9ed5b56e8534e7d360a01f0c15b8 🚀

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.CVV4mm3u.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.BKhusWgz.js";import{_ as a}from"../app.CnXcxaNY.js";import"./chunk-5VM5RSS4.CwhnSkdm.js";import"./chunk-XXDRQBXY.DAxtDa2j.js";import"./chunk-KBJHAD2P.Z1GQ6sBP.js";import"./chunk-2GRJ4B5K.CTC6p0hm.js";import"./channel.DpHM3Kuk.js";import"./framework.FXkbE5vR.js";import"./theme.vTMCfCjv.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
