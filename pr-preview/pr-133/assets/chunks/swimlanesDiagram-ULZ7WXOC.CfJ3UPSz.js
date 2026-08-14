f723439ccb8629b6f1db12d8442a8bedffd1047e
gkrajniak <gkrajniak@users.noreply.github.com>
2026-08-14 12:40:27 +0000

    Remove preview for PR 133 🛬

--- pr-preview/pr-133/assets/chunks/swimlanesDiagram-ULZ7WXOC.CfJ3UPSz.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.C3s2mFjG.js";import{_ as a}from"../app.Cc3YT6uD.js";import"./chunk-5VM5RSS4.BBWN-mV9.js";import"./chunk-XXDRQBXY.Ch-TbFJS.js";import"./chunk-KBJHAD2P.BmwlzN4b.js";import"./chunk-2GRJ4B5K.Q6KVLC13.js";import"./channel.BxaiCYbB.js";import"./framework.BM2Q-X0V.js";import"./theme.A7SqzTiW.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
